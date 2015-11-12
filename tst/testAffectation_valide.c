d0f13acf795640c38d4b51bbca9e3ef767e08551
elraine <laevatinn@gmail.com>
2015-11-12 18:56:39 +0100

    t1

---
+++ tst/testAffectation_valide.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main{
+
+  //Affectations types de base
+  int i = 3;
+  float f = 3.5;
+  char c = a;
+
+  //Affectations tableau types de base
+  int tabi[10];
+  float tabf[10];
+  char tabc[10];
+
+
+
+
+}
