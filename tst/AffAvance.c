46b93792c0e7b8a3519aafa9bf2cba776b19fb53
maschmalow <maschmalow@gmail.com>
2015-11-13 11:53:02 +0100

    tests

--- tst/AffAvance.c
+++ tst/AffAvance.c
@@ -3,17 +3,23 @@
 int main()
 {
 
-    void (*f1)(char[], float)[10];
+  void (*)(char[], float) tab1[5]; //un tableau de 5 fonctions qui prennent un tableau de char et un float et qui ne renvoient rien.
 
-    float[10] (*f2)();
+    float (*f2)()[];
     
-    char[50] (*f3)(int[10])[]; //un tableau vide de fonctions qui prennent un tableau de 10 int et qui renvoient un tableau de 50 char.
+    char (*)(int[10])[50] tab[4]; //un tableau de 4 fonctions qui prennent un tableau de 10 int et qui renvoient un tableau de 50 char.
 
+    float tabf[];
+    
+    char (*f)(int[10])[50];
 
+    int x[20];
 
+    tabf = f2();
 
+    tab[0] = f;
 
-
+    tab1[0](tab[2](x));
 
     return 0;
 }
---
+++ tst/wrongVoid.c
@@ -0,0 +1,11 @@
+//Ne dois PAS compiler, void invalide
+
+int main()
+{
+
+  int (*f)(void);
+
+  void tab[50];
+
+  return 0;
+}
