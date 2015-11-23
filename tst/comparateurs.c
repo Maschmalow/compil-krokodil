a6aa2c1ee86a4bcf3a39217fa2c47d14c0a2ce59
elraine <laevatinn@gmail.com>
2015-11-23 15:57:33 +0100

    doc + tst

---
+++ tst/comparateurs.c
@@ -0,0 +1,17 @@
+//comparateurs entiers
+
+
+int main(){
+int i;
+int j;
+int k;
+int r;
+int tab[];
+
+i = 2;
+j = 3;
+k=3;
+
+r = (i < j)+(i > -j)+(i <= j)+(i>=-j)+(j==k)+(i!=k);
+return r;
+}
---
+++ tst/wrongOperations.c
@@ -0,0 +1,13 @@
+//Ne dois PAS compiler, operations invalides
+
+int main {
+  int i;
+  int j;
+  float f;
+  float g;
+
+  i = 10/3; //i n'est pas un float
+  i = i/f; // i n'est pas un float
+
+
+}
