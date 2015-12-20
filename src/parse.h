a8ad3ed730b70f25842c902cff6594283fa809e9
Maschmalow <maschmalow@gmail.com>
2015-12-20 22:14:44 +0100

    a

--- src/parse.h
+++ src/parse.h
@@ -8,7 +8,7 @@
 
 extern int cur_depth;   
 extern var_lmap* cur_vars;
-extern var_s* cur_func_params;
-extern var_s* pending_vars;
+extern var_s** cur_func_params;
+extern var_s** pending_vars;
 
 #endif
