c4688746718e73197a7bd43cd6e0c4c1c1b646fc
elraine <laevatinn@gmail.com>
2015-12-20 18:31:38 +0100

    rrr

--- runTests.sh
+++ runTests.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
+echo "cooking the executable"
+cd src
+make
+cd ..
+echo "cooking is over"
 echo "Tests compilations" > analysis.out
 date >> analysis.out
 for file in ./tst/*
 do
+  echo "$file"
   echo "$file" >>analysis.out
   ./src/krokodil "$file" >> analysis.out
 done
