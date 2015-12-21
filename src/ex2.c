c91fddc427e58f35a9f46b320427f0748abd8b70
Maschmalow <maschmalow@gmail.com>
2015-12-21 01:35:36 +0100

    a

--- src/ex2.c
+++ src/ex2.c
@@ -2,12 +2,14 @@ int main() {
   int A[1000];
   int i;
   int x;
+  int tmp;
   x=0;
   do 
     x++;
   while( x <= 15 );
   for (i=0; i<1000; i++) {
-    A[i]=i;
+  tmp = A[i];
+    tmp=i;
   }
   for (i=0; i<1000; i++) {
     x+=A[i]*A[i];
